b025c53ad52f65f5432f67ec10dd777f81d79cbe
Dinocanid <AlexiaW2D@gmail.com>
2018-08-15 13:52:08 -0400

    Add files via upload

---
+++ js/acp.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ 
+	//ACCORDION BUTTON ACTION	
+	$('div.accordionButton').click(function() {
+		$('div.accordionContent').slideUp('normal');	
+		$(this).next().slideDown('normal');
+	});
+ 
+	//HIDE THE DIVS ON PAGE LOAD	
+	$("div.accordionContent").hide();
+ 
+});
