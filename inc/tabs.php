48366db79da3bf9059b459c4c3ae3b38afd3db9e
Dinocanid <AlexiaW2D@gmail.com>
2018-08-15 14:21:07 -0400

    Add files via upload

---
+++ inc/tabs.php
@@ -0,0 +1,7 @@
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js'></script>
+<script src="../../js/tabs.js"></script>
+<script>
+    $(function() {   
+        $("#profile").organicTabs();            
+    });
+</script>
