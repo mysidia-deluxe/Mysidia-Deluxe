12fb575274ce0872468ec7858e04a8da2c482f41
King-of-Crows <42444246+King-of-Crows@users.noreply.github.com>
2018-08-22 10:46:03 -0400

    Update daycare function
    Modifications to daycare function for optimization.

--- view/levelupview.php
+++ view/levelupview.php
@@ -28,37 +28,48 @@ class LevelupView extends View{
 	
 	}
 	
-	public function daycare(){
-		$mysidia = Registry::get("mysidia");
-		$document = $this->document;	
+	    public function daycare(){
+    //    $mysidia = Registry::get("mysidia");
+        $document = $this->document;
         $document->setTitle($this->lang->daycare_title);
         $document->addLangvar($this->lang->daycare, TRUE);
-		
-		$daycare = $this->getField("daycare");
-        $adopts = $daycare->getAdopts();
-		$daycareTable = new Table("daycare", "", FALSE);
-		$daycareTable->setBordered(FALSE);
-		$total = $daycare->getTotalAdopts();
+
+        $daycare = $this->getField("daycare");
+    //    $adopts = $daycare->getAdopts();
+        $daycareTable = new Table("daycare", "", FALSE);
+        $daycareTable->setBordered(FALSE);
+
+        // New method call
+        $adopts = $daycare->joinedListing();
+
+        $total = $daycare->getTotalAdopts();
         $index = 0;
 
-		for($row = 0; $row < $daycare->getTotalRows(); $row++){
-	        $daycareRow = new TRow("row{$row}");
+        for($row = 0; $row < $daycare->getTotalRows(); $row++){
+            $daycareRow = new TRow("row{$row}");
             for($column = 0; $column < $daycare->getTotalColumns(); $column++){
-			    $adopt = new OwnedAdoptable($adopts[$index]);
-				$cell = new ArrayList;
-				$cell->add(new Link("levelup/click/{$adopt->getAdoptID()}", $adopt->getImage("gui"), TRUE));
-				$cell->add(new Comment($daycare->getStats($adopt)));
-				$daycareCell = new TCell($cell, "cell{$index}");
+            //  $adopt = new OwnedAdoptable($adopts[$index]);
+
+                $adopt = $adopts[$index];
+                $cell = new ArrayList;
+            //    $cell->add(new Link("levelup/click/{$adopt->getAdoptID()}", $adopt->getImage("gui"), TRUE));
+            //    $cell->add(new Comment($daycare->getStats($adopt)));
+
+                // New display calls.
+                $cell->add(new Link("levelup/click/{$adopt['aid']}", "<img src='{$daycare->getImageFromArray($adopt)}'>", TRUE));
+                $cell->add(new Comment($daycare->getStatsFromArray($adopt)));
+
+                $daycareCell = new TCell($cell, "cell{$index}");
                 $daycareCell->setAlign(new Align("center", "center"));
-				$daycareRow->add($daycareCell);
-				$index++;
-				if($index == $total) break;
+                $daycareRow->add($daycareCell);
+                $index++;
+                if($index == $total) break;
             }
-            $daycareTable->add($daycareRow);			
-		}
-		
+            $daycareTable->add($daycareRow);
+        }
+
         $document->add($daycareTable);
-		if($pagination = $daycare->getPagination()) $document->addLangvar($pagination->showPage());
-	}
+        if($pagination = $daycare->getPagination()) $document->addLangvar($pagination->showPage());
+    } 
 }
 ?>
