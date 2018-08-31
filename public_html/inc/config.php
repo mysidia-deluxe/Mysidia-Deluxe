9a28b90be66398fea55155b3d983321c71bed384
Andrew Judd <awjudd@gmail.com>
2018-08-31 00:29:23 -0400

    Reverting the config file

--- public_html/inc/config.php
+++ public_html/inc/config.php
@@ -1,11 +1,10 @@
 <?php
 //Mysidia Adoptables Site Configuration File
-
-define('DBHOST', '127.0.0.1');             //DB Hostname
+define('DBHOST', 'localhost');             //DB Hostname
 define('DBUSER', 'root');             //DB Username
-define('DBPASS', '');             //DB Password
-define('DBNAME', 'mysidia-adoptables');             //Your database name
-define('DOMAIN', 'mysidia.dev');             //Your domain name (No http, www or . )
-define('SCRIPTPATH', '');     //The folder you installed this script in
+define('DBPASS', 'password');             //DB Password
+define('DBNAME', 'mys_deluxe');             //Your database name
+define('DOMAIN', '127.0.0.1');             //Your domain name (No http, www or . )
+define('SCRIPTPATH', '/mys_deluxe');     //The folder you installed this script in
 define('PREFIX', 'adopts_');
 ?>
