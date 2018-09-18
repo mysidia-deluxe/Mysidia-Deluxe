98c92912164f8949a0b37439fa7b6dfb27705626
Dinocanid <AlexiaW2D@gmail.com>
2018-09-17 21:59:19 -0400

    gitignore fixes

--- public_html/inc/config.php
+++ public_html/inc/config.php
@@ -3,7 +3,7 @@
 
 define('DBHOST', 'localhost');             //DB Hostname
 define('DBUSER', 'root');             //DB Username
-define('DBPASS', 'chibidog');             //DB Password
+define('DBPASS', 'password');             //DB Password
 define('DBNAME', 'mys_deluxe');             //Your database name
 define('DOMAIN', 'localhost');             //Your domain name (No http, www or . )
 define('SCRIPTPATH', '/Mysidia-Deluxe/public_html');     //The folder you installed this script in
